e34a5733fa4a45589b14b12254844149ae22ce87
sriramk1n1 <sriramjkini@gmail.com>
2023-09-03 14:09:11 +0530

    Revert "prerender"
    This reverts commit 11f2f01cccb3eac95a79f18ed258e08c8dd8654e.

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.2bda41ed.js","_app/immutable/chunks/scheduler.9fa682d6.js","_app/immutable/chunks/index.42ddef74.js"];
-export const stylesheets = [];
-export const fonts = [];
